895f4569bb9a35f359463027dff6c2978e982ed7
valentinbreiz <valentinbreiz@gmail.com>
2018-05-19 23:52:33 +0200

    Added 5.05 kernel offsets + pmap_protect patch fix for 5.05. (#14)
    * Update kernel.c
    
    * Update kernel.c
    
    * Some 5.05 offsets added.
    
    * More 5.05 offsets.
    
    * More offsets added!

--- kernel.c
+++ kernel.c
@@ -250,11 +250,19 @@ static int patch_pmap_check(void)
 
     for (p = (u8*)kern.pmap_protect;
          p < ((u8*)kern.pmap_protect + 0x500); p++) {
-        if (!memcmp(p, "\x83\xe0\x06\x83\xf8\x06", 6)) {
+        #ifdef PS4_5_05
+        if (!memcmp(p, "\xB8\x06\x00\x00\x00\xC4", 6)) { 
+            p[1] = 0;
+            kern.printf("pmap_protect patch successful (found at %p)\n", p);
+            return 1;
+        }
+        #else
+        if (!memcmp(p, "x83\xe0\x06\x83\xf8\x06", 6)) {
             p[2] = 0;
             kern.printf("pmap_protect patch successful (found at %p)\n", p);
             return 1;
         }
+        #endif
     }
     kern.printf("pmap_protect patch failed!\n");
     return 0;
--- magic.h
+++ magic.h
@@ -150,4 +150,30 @@
 #define kern_off_dmpml4i 0x22CB4E4
 #define kern_off_dmpdpi 0x22CB4E8
 
+#elif defined PS4_5_05
+
+#define kern_off_printf 0x436040
+#define kern_off_copyin 0x1ea710
+#define kern_off_copyout 0x1ea630
+#define kern_off_copyinstr 0x1eab40
+#define kern_off_kmem_alloc_contig 0xF1C90
+#define kern_off_kmem_free 0xfce50
+#define kern_off_pmap_extract 0x2E0570
+#define kern_off_pmap_protect 0x2E3090
+#define kern_off_sched_pin 0x31ff40
+#define kern_off_sched_unpin 0x31ff50
+#define kern_off_smp_rendezvous 0x1B85B0
+#define kern_off_smp_no_rendevous_barrier 0x1B8366
+#define kern_off_icc_query_nowait 0x44020
+#define kern_off_kernel_map 0x1AC60E0
+#define kern_off_sysent 0x0107c610
+#define kern_off_kernel_pmap_store 0x22CB570
+#define kern_off_Starsha_UcodeInfo 0
+#define kern_off_gpu_devid_is_9924 0x4DE010
+#define kern_off_gc_get_fw_info 0x4D37A0
+
+#define kern_off_pml4pml4i 0x22CB560 // Pending verification.
+#define kern_off_dmpml4i 0x22CB564
+#define kern_off_dmpdpi 0x22CB568
+
 #endif
