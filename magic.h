2e8c3c90166ea4e8f6cb5670bbf4148805abcd1e
ethylamine <46206558+ethylamine@users.noreply.github.com>
2019-02-11 19:36:10 -0500

    Added offsets for 4.55 compatability

--- magic.h
+++ magic.h
@@ -101,6 +101,7 @@
 #elif defined PS4_4_55
 
 #define kern_off_printf 0x17F30
+#define kern_off_snprintf 0x18230 //newly added
 #define kern_off_copyin 0x14A890
 #define kern_off_copyout 0x14A7B0
 #define kern_off_copyinstr 0x14AD00
@@ -123,6 +124,41 @@
 #define kern_off_pml4pml4i 0x21BCC28
 #define kern_off_dmpml4i 0x21BCC2C
 #define kern_off_dmpdpi 0x21BCC30
+
+#define kern_off_eap_hdd_key 0x258CCD0 //newly added
+#define kern_off_edid 0x2613330 //newly added
+#define kern_off_wlanbt 0x345E0 //newly added
+
+#elif defined PS4_4_74
+
+#define kern_off_printf                     0x17F30
+#define kern_off_snprintf 							    0x18230
+#define kern_off_copyin                     0x149f20
+#define kern_off_copyout                    0x149E40
+#define kern_off_copyinstr                  0x14A390
+#define kern_off_kmem_alloc_contig          0x2527A0
+#define kern_off_kmem_free                  0x16E100
+#define kern_off_pmap_extract               0x41EA30
+#define kern_off_pmap_protect               0x421180
+#define kern_off_sched_pin                  0x72D50
+#define kern_off_sched_unpin                0x72D60
+#define kern_off_smp_rendezvous             0xB2210
+#define kern_off_smp_no_rendevous_barrier   0xB1FD0
+#define kern_off_icc_query_nowait           0x7FF00
+#define kern_off_kernel_map                 0x1B39218
+#define kern_off_sysent                     0x1034790
+#define kern_off_kernel_pmap_store          0x21C5A38
+#define kern_off_Starsha_UcodeInfo			0
+#define kern_off_gpu_devid_is_9924          0x4976A0
+#define kern_off_gc_get_fw_info             0x4A2250
+
+#define kern_off_pml4pml4i                  0x21C5A28
+#define kern_off_dmpml4i                    0x21C5A2C
+#define kern_off_dmpdpi                     0x21C5A30
+
+#define kern_off_eap_hdd_key                0x259CCD0
+#define kern_off_edid                       0x26236D0
+#define kern_off_wlanbt                     0x33BC0
 
 #elif defined PS4_5_01
 
