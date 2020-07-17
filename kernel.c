6aa01f6e3bda1a55b591a76a5d8424ee13ef601d
ethylamine <46206558+ethylamine@users.noreply.github.com>
2020-07-17 06:23:41 +0000

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -254,13 +254,12 @@ static int patch_pmap_check(void)
 
     for (p = (u8*)kern.pmap_protect;
          p < ((u8*)kern.pmap_protect + 0x500); p++) {
-        #ifdef PS4_4_55 // changed to 55 from 74
+        #ifdef PS4_6_72
         if (!memcmp(p, "\xB8\x06\x00\x00\x00\xC4", 6)) {
             p[1] = 0;
             kern.printf("pmap_protect patch successful (found at %p)\n", p);
             return 1;
         }
-            
         /*#ifdef PS4_5_05
         if (!memcmp(p, "\xB8\x06\x00\x00\x00\xC4", 6)) {
             p[1] = 0;
